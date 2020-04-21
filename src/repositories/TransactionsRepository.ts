edb592cff81fea42cb8cf5b12e2e404db7b55bb6
Carlos Berti <carlosberti1@hotmail.com>
2020-04-20 22:12:29 -0300

    Implement functions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (accumulator, current) => {
+        if (current.type === 'income') {
+          accumulator.income += current.value;
+          accumulator.total += current.value;
+        } else {
+          accumulator.outcome += current.value;
+          accumulator.total -= current.value;
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    const balance = this.getBalance();
+
+    if (balance.total <= value && type === 'outcome') {
+      throw Error('Balance not sufficient');
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
